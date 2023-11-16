fff518d72446ab7470e587d94cbb5ae6c17b7f67
tuannt02 <20522122@gm.uit.edu.vn>
2023-11-16 22:24:54 +0700

    add category entity

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,49 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity({ name: 'category' })
+export class Category {
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'description', nullable: true })
+  description: string;
+
+  @Column({ name: 'level', nullable: false })
+  level: number;
+
+  @ManyToOne(() => Category, (category) => category.childCategories)
+  @JoinColumn({
+    name: 'parent_id',
+    referencedColumnName: 'id',
+  })
+  parentCategory: Category;
+
+  @OneToMany(() => Category, (category) => category.parentCategory)
+  childCategories: Category[];
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  updatedAt: Date;
+}
